16c00756133839d15fa08cd22e7d7bc6bd215838
Rahul Ingle <inglerahul2@gmail.com>
2020-05-23 19:03:12 +0530

    Adding new Projects and Adding ListUsers

---
+++ Test Suites/API Test Suites/Users/ListUsers.ts
@@ -0,0 +1,31 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>ListUsers</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>3c53a948-80d8-4ec1-8bcc-308b8adfef07</testSuiteGuid>
+   <testCaseLink>
+      <guid>588d312e-5b7c-4bc4-b34d-a4e7261819c3</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Users/ListUsers/TC01_ListUsers_Valid</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>9f4b4fa5-f117-4168-b987-f90ede5fcff3</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Users/ListUsers/TC02_ListUsers_InvalidAuth</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>3873ee3e-cba3-4a4d-b692-aaabaaa3d754</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Users/ListUsers/TC03_ListUsers_WithoutAuth</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
